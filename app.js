8b2938f5c2e51a79a9931ccfbc5cea1b1802707a
Manny Navarro <manny.navarro@gmail.com>
2015-06-29 12:41:35 -0600

    messing around in app.js with prototypes

--- app.js
+++ app.js
@@ -12,9 +12,24 @@
 //   alert(nameList);
 
 // Arrays
-var arrayName = new Array(10, 20, 40, 4000, 200, "Dude!");
+// var arrayName = new Array(10, 20, 40, 4000, 200, "Dude!");
+//
+// alert(arrayName[3]);
+// alert(arrayName[1]);
+// alert(arrayName[0]);
+// alert(arrayName[5]);
 
-alert(arrayName[3]);
-alert(arrayName[1]);
-alert(arrayName[0]);
-alert(arrayName[5]);
+// Define a repeatify function on the String object. The function accepts an integer
+// that specifies how many times the string has to be repeated. The function returns
+// the string repeated the number of times specified. For example: console.log('hello'.repeatify(3));
+
+String.prototype.repeatify = String.prototype.repeatify || function(times){
+  var str = '';
+
+  for(var i=0; i<times; i++){
+    str +=this;
+  }
+  return str;
+};
+
+console.log('hello'.repeatify(3));
