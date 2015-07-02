7190d59dae74a336b6977a46e23a4e16ed0734c6
Manny Navarro <manny.navarro@gmail.com>
2015-07-02 07:00:23 -0600

    added sweetalert component - nice alert box confirmation

--- loop_array_checking.js
+++ loop_array_checking.js
@@ -4,7 +4,11 @@ var userInput = prompt("What is your city?");
 
 for(var i = 0; i <= bestCities.length; i++) {
   if(userInput === bestCities[i]) {
-    alert("Congratulations!!! You live in a kick-ass city.");
-  } else 
-    alert("Wow! Your city sucks, I am so sad for you!");
+    // alert("Congratulations!!! You live in a kick-ass city.");
+    swal({   title: "Awesome!",   text: "Congratulations!!! You live in a kick-ass city.",   type: "error",   confirmButtonText: "Cool" });
+    break;
+  } else
+    // alert("Wow! Your city sucks, I am so sad for you!");
+    swal({   title: "Sorry",   text: "Wow! Your city sucks, I am so sad for your!",   type: "error",   confirmButtonText: "Cool" });
+    break;
 }
--- loop_flags_booleans.js
+++ loop_flags_booleans.js
@@ -6,10 +6,15 @@ var matchFound = false;
 for(var i = 0; i <= bestCities.length; i++) {
   if(userInput === bestCities[i]) {
     matchFound = true;
-    alert("Congratulations!!! You live in a kick-ass city.");
+    // alert("Congratulations!!! You live in a kick-ass city.");
+    swal({
+      title: "Awesome!",
+      text: "Congratulations!!! You live in a kick-ass city.",
+      type: "error",
+      confirmButtonText: "Cool" });
     break;
-  } else {
-    alert("Wow! Your city sucks, and I am so sad for you!");
+  } else
+    // alert("Wow! Your city sucks, I am so sad for you!");
+    swal({   title: "Sad!",   text: "Wow! Your city sucks, I am so sad for your!",   type: "error",   confirmButtonText: "Cool" });
     break;
-}
 }
